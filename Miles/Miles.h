c9a2559d9ecc9940a724700068bfc7565f72b3c9
Scott Petit <scott@hecho.io>
2015-12-11 06:32:28 -0600

    Attempts to fix broken OSX build

--- Miles/Miles.h
+++ Miles/Miles.h
@@ -6,7 +6,7 @@
 //  Copyright © 2015 Tastemade. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Miles.
 FOUNDATION_EXPORT double MilesVersionNumber;
